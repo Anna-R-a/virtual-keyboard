94a380d310d4a21453191449aabe01166a2e8a24
Anna-R-a <ratnikova_a.m@mail.ru>
2023-05-01 12:52:23 +0300

    feat: add favicon and improve figure of keyboard

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// import { createObject, codeKeys, keys } from './create';
+// import { createObject, codeKeys, keys } from "./create";
 
 function createObject(tag, className) {
   const element = document.createElement(tag);
@@ -11,63 +11,53 @@ const codeKeys = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5',
   'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
   'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 
-const keys = ['Escape', '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-  'Tab', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete',
+const keys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
   'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
-  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift',
-  'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#8593;', 'Shift',
+  'Ctrl', 'Win', 'Alt', ' ', 'Alt', '&#8592;', '&#8595;', '&#8594;', 'Ctrl'];
 
 const text = createObject('textarea', 'text');
 text.setAttribute('id', 'text');
 document.body.appendChild(text);
 
-const keyboard = createObject('div', 'keyboard');
-keyboard.setAttribute('id', 'keyboard');
-document.body.appendChild(keyboard);
-
-function init() {
-  let symbol = '';
-  for (let i = 0; i < keys.length; i += 1) {
-    if (keys[i] === 'Backspace' || keys[i] === 'Delete'
-      || keys[i] === 'Enter' || keys[i] === 'Shift') {
-      symbol += '<div class="clear"></div>';
-    }
-    symbol += `<div class="keyboard__key" data = ${keys[i]}> ${keys[i]} </div>`;
-    document.querySelector('#keyboard').innerHTML = symbol;
-  }
-}
-
 function createKeyboard() {
-  // const keyboard = createObject('div', 'keyboard');
-  // document.body.appendChild(keyboard);
-  init();
+  const keyboard = createObject('div', 'keyboard');
+  document.body.appendChild(keyboard);
   // keyboard.appendChild(keyBtn);
   codeKeys.forEach((key) => {
-    // const keyBtn = createObject('button', 'keyboard__key');
+    // for (let i = 0; i < keys.length; i += 1) {
+    const keyBtn = createObject('button', 'keyboard__key');
     // keyBtn.setAttribute = ('type', 'button');
-    // const newLine = ['Backspace', 'Delete', 'Enter', 'ShiftRight'].indexOf(key) !== -1;
-    if (key === 'Backspace' || key === 'Tab' || key === 'CapsLock'
-    || key === 'Enter' || key === 'ShiftLeft' || key === 'ShiftRight') {
-      document.querySelector('.keyboard .keyboard__key').classList.add('keyboard__key_long');
-    }
+    keyBtn.setAttribute = ('data', `${codeKeys[codeKeys.indexOf(key)]}`);
     if (key === 'Space') {
-      document.querySelector('.keyboard .keyboard__key').classList.add('keyboard__key_space');
+      keyBtn.classList.add('keyboard__key_space');
+      keyBtn.innerHTML = '';
     }
+    if (key === 'Backspace') {
+      keyBtn.classList.add('keyboard__key_long');
+      keyBtn.innerHTML = 'Backspace';
+    }
+    if (key === 'Tab') {
+      // keyBtn.classList.add('keyboard__key_long');
+      keyBtn.innerHTML = 'Tab';
+    }
+    if (key === 'CapsLock') {
+      keyBtn.classList.add('keyboard__key_long');
+      keyBtn.innerHTML = 'CapsLock';
+    }
+    if (key === 'Enter') {
+      keyBtn.classList.add('keyboard__key_long');
+      keyBtn.innerHTML = 'Enter';
+    }
+    if (key === 'ShiftLeft' || key === 'ShiftRight') {
+      keyBtn.classList.add('keyboard__key_long');
+      keyBtn.innerHTML = 'Shift';
+    }
+    keyBtn.innerHTML = `${keys[codeKeys.indexOf(key)]}`;
+    keyboard.appendChild(keyBtn);
   });
 }
-
-createKeyboard();
-
-// function init() {
-//   let symbol = '';
-//   for (let i = 0; i < keys.length; i += 1) {
-//     if (keys[i] === 'Backspace' || keys[i] === 'Delete'
-//       || keys[i] === 'Enter' || keys[i] === 'Shift') {
-//       symbol += '<div class="clear"></div>';
-//     }
-//     symbol += `<div class="keyboard__key" data = ${keys[i]}> ${keys[i]} </div>`;
-//   }
-//   document.querySelector('#keyboard').innerHTML = symbol;
-// }
-// init();
-// }
+window.addEventListener('DOMContentLoaded', () => {
+  createKeyboard();
+});
